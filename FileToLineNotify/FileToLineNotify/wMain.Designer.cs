000db706bed4f4a8f2c2cb0688ef1818d4c30f8a
Phakin <jabavok.007@gmail.com>
2018-11-03 18:08:09 +0700

    CngUp

---
+++ FileToLineNotify/FileToLineNotify/wMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FileToLineNotify
+{
+    partial class wMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ocmSend = new System.Windows.Forms.Button();
+            this.ocmOpenFile = new System.Windows.Forms.Button();
+            this.otbPartFile = new System.Windows.Forms.TextBox();
+            this.oOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.otbResult = new System.Windows.Forms.TextBox();
+            this.otbMsgRes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ocmSend
+            // 
+            this.ocmSend.Location = new System.Drawing.Point(25, 85);
+            this.ocmSend.Name = "ocmSend";
+            this.ocmSend.Size = new System.Drawing.Size(75, 28);
+            this.ocmSend.TabIndex = 0;
+            this.ocmSend.Text = "Send";
+            this.ocmSend.UseVisualStyleBackColor = true;
+            this.ocmSend.Click += new System.EventHandler(this.ocmSend_Click);
+            // 
+            // ocmOpenFile
+            // 
+            this.ocmOpenFile.Location = new System.Drawing.Point(266, 52);
+            this.ocmOpenFile.Name = "ocmOpenFile";
+            this.ocmOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.ocmOpenFile.TabIndex = 1;
+            this.ocmOpenFile.Text = "OpenFile";
+            this.ocmOpenFile.UseVisualStyleBackColor = true;
+            this.ocmOpenFile.Click += new System.EventHandler(this.ocmOpenFile_Click);
+            // 
+            // otbPartFile
+            // 
+            this.otbPartFile.Location = new System.Drawing.Point(26, 52);
+            this.otbPartFile.Name = "otbPartFile";
+            this.otbPartFile.Size = new System.Drawing.Size(234, 20);
+            this.otbPartFile.TabIndex = 2;
+            // 
+            // otbResult
+            // 
+            this.otbResult.Location = new System.Drawing.Point(26, 125);
+            this.otbResult.Multiline = true;
+            this.otbResult.Name = "otbResult";
+            this.otbResult.Size = new System.Drawing.Size(234, 52);
+            this.otbResult.TabIndex = 3;
+            // 
+            // otbMsgRes
+            // 
+            this.otbMsgRes.Location = new System.Drawing.Point(278, 125);
+            this.otbMsgRes.Multiline = true;
+            this.otbMsgRes.Name = "otbMsgRes";
+            this.otbMsgRes.Size = new System.Drawing.Size(99, 52);
+            this.otbMsgRes.TabIndex = 4;
+            // 
+            // wMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 189);
+            this.Controls.Add(this.otbMsgRes);
+            this.Controls.Add(this.otbResult);
+            this.Controls.Add(this.otbPartFile);
+            this.Controls.Add(this.ocmOpenFile);
+            this.Controls.Add(this.ocmSend);
+            this.Name = "wMain";
+            this.Load += new System.EventHandler(this.wMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ocmSend;
+        private System.Windows.Forms.Button ocmOpenFile;
+        private System.Windows.Forms.TextBox otbPartFile;
+        private System.Windows.Forms.OpenFileDialog oOpenFileDialog;
+        private System.Windows.Forms.TextBox otbResult;
+        private System.Windows.Forms.TextBox otbMsgRes;
+    }
+}
+
---
+++ FileToLineNotify/FileToLineNotify/wMain.cs
@@ -0,0 +1,129 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileToLineNotify
+{
+    public partial class wMain : Form
+    {
+        public wMain()
+        {
+            InitializeComponent();
+        }
+
+        private void wMain_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ocmOpenFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog oOpenFileDialog = new OpenFileDialog()
+                {
+                    Multiselect = true,
+                };
+                if (oOpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    otbPartFile.Text = oOpenFileDialog.FileName;
+                }
+            }
+            catch (Exception oEx)
+            {
+                MessageBox.Show("wMain : ocmOpenFile_Click // " + oEx.Message);
+            }
+        }
+        private void ocmSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (otbPartFile.Text.Equals(""))
+                {
+                    MessageBox.Show("Select File");
+                }
+                else
+                {
+                    ImageUploadResult oUploadResult = GEToUrl();
+                    otbResult.Text = oUploadResult.Uri.OriginalString;
+                    SETxPictureToLinNotify(otbResult.Text);
+                }
+            }
+            catch (Exception oEx)
+            {
+                MessageBox.Show("wMain : ocmSend_Click // " + oEx.Message);
+            }
+        }
+        private ImageUploadResult GEToUrl()
+        {
+            try
+            {
+                Cloudinary oCloudinary = new Cloudinary(new Account("jabavokza", "978885474333712", "tjSIxPOssmN7F5cqnCbW_b0n3Jk"));
+
+                ImageUploadParams oUploadParams = new ImageUploadParams()
+                {
+                    File = new FileDescription(otbPartFile.Text),
+                };
+                ImageUploadResult oUploadResult = oCloudinary.Upload(oUploadParams);
+                return oUploadResult;
+            }
+            catch (Exception oEx)
+            {
+                throw oEx;
+            }
+        }
+
+        private void SETxPictureToLinNotify(string ptUrl)
+        {
+            try
+            {
+                string tMsg = "Code I have";// ข้อความแนบรูปภาพ  สามารถเปลี่ยนได้
+                otbMsgRes.Text = POStToApiLine(tMsg, ptUrl);
+            }
+            catch (Exception oEx)
+            {
+                MessageBox.Show("wMain : SETxPictureToLinNotify // " + oEx.Message);
+            }
+
+        }
+        private string POStToApiLine(string ptMsg, string ptPictureUrl)
+        {
+            try
+            {
+                string tToken = "rIZaHJMClZ2dAqJwO8v96WWjmAEEiEi1qHvwRZezodF";
+                HttpWebRequest oRequest = (HttpWebRequest)WebRequest.Create("https://notify-api.line.me/api/notify");
+
+                string tPostData = string.Format("message={0}", ptMsg);
+                if (ptPictureUrl != "")
+                {
+                    string tImageThumbnail = string.Format("imageThumbnail={0}", ptPictureUrl);
+                    string tImageFullsize = string.Format("imageFullsize={0}", ptPictureUrl);
+                    tPostData += "&" + tImageThumbnail.ToString() + "&" + tImageFullsize.ToString();
+                }
+                byte[] aData = Encoding.UTF8.GetBytes(tPostData);
+
+                oRequest.Method = "POST";
+                oRequest.ContentType = "application/x-www-form-urlencoded";
+                oRequest.ContentLength = aData.Length;
+                oRequest.Headers.Add("Authorization", "Bearer " + tToken);
+
+                using (Stream oStream = oRequest.GetRequestStream())
+                {
+                    oStream.Write(aData, 0, aData.Length);
+                }
+
+                HttpWebResponse oResponse = (HttpWebResponse)oRequest.GetResponse();
+                string tResponseString = new StreamReader(oResponse.GetResponseStream()).ReadToEnd();
+                return tResponseString;
+            }
+            catch (Exception oEx)
+            {
+                throw oEx;
+            }
+        }
+
+    }
+}
